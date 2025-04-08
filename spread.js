e2387d37970aab6a508c8cb3b90680bd7c4fb774
Harsh-P30 <harshprasad1204@gmal.com>
2025-04-08 16:34:27 +0530

    how to use spread with  object and function

--- spread.js
+++ spread.js
@@ -12,7 +12,7 @@ To pass multiple values (e.g., into a function)
 
 To avoid mutation of original data (immutable code)  */
 
-// Using Spread Operator
+// Using Spread Operator with Array
 const a =[1,2,3,4,5];
 const b =[1,2,3,4,5];
 const copy = [...a,...b];
@@ -21,3 +21,20 @@ console.log(copy);
 const text ="asdf"
 const nee = [...text];
 console.log(nee)
+
+// . Spread with Objects
+const user = {
+    name :"harsh",
+    age:"20"
+}
+const userData ={...user};     // we are spreading object so we have to store data into object form
+console.log(userData)
+
+//  Use Spread in Functions
+
+function sum(a,b,c,d){
+    return a+b+c+d;
+}
+
+const num =[1,2,3,4];
+console.log(sum(...num));
