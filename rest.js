eddda91d57ffe20aed2a85b2adfd2b2aac85a031
Harsh-P30 <harshprasad1204@gmal.com>
2025-04-08 15:45:17 +0530

    learn rest

--- rest.js
+++ rest.js
@@ -2,8 +2,12 @@
 //     // code that uses restParameter
 //   }
 
-//rest operator combines the values of argument as input into a array. 
+//rest operator combines the values of argument as input into a array.
+//In traditional JavaScript functions, there was an arguments object that stores all passed arguments. However, the arguments object is not an array, so you couldn’t directly use array methods on it.
 
+//Rest parameters, on the other hand, are an actual array, making it more useful and easier to work with. 
+
+/*
 let sum = 0;
 let sum2 = 0;
 function add(a, b, ...c) {
@@ -20,3 +24,8 @@ function add(a, b, ...c) {
 }
 
 console.log(add(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+
+
+*/
+
