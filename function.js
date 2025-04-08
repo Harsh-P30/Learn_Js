4cab838e2e1084c530ef2d8600503b473c7ae7a2
Harsh-P30 <harshprasad1204@gmal.com>
2025-04-08 16:05:13 +0530

    rephrase Something and

--- function.js
+++ function.js
@@ -1,4 +1,6 @@
-function people(name) {
+ // Known as Function declaration
+// ✔️ Hoisted: You can call it before it's defined.
+function people(name) { 
     console.log("I am named function" + name)
 }
 people("  hello");  //A function is declared using the function keyword and can be called before or after the declaration.
@@ -10,14 +12,21 @@ const fun = (people) => {  //Arrow function is a shorter syntax for writing func
 
 fun(people("  and called by other function"));
 
+
+
 (function () {  //A function that executes immediately after definition.
     console.log("immediately invoked function")
 })();
 
-const Anonymous = function () {
+
+// Function Expression
+// A Function Expression involves assigning a function to a variable.
+// ❌ Not Hoisted: You cannot call it before it's defined.  because it store into a variable
+const Anonymous = function add() {
     console.log("i am a anonymous function")
 }
 Anonymous();
+// add(); // add is not defined you have to call using stored varible name
 
 
 // JavaScript default parameters in "ES6".
