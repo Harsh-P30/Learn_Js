3ba9a330305f46a59978932e6b77570ac70e4bb0
Your Name <you@example.com>
2025-04-23 00:28:15 +0530

    new thing in scope learn about (block scope)

--- scope.js
+++ scope.js
@@ -6,5 +6,17 @@ function functionScope(){
 }
 
 functionScope();
-name;  // gives the error because it is not into the scope or in lamen language we can't reach to 'name';
+// name;  // gives the error because it is not into the scope or in lamen language we can't reach to 'name';
 
+
+
+
+
+
+// block scope
+
+{
+    let x=10;
+    console.log(x);
+}
+// console.log(x);   // accessing out of block so it will give you ReferenceError: x is not defined
