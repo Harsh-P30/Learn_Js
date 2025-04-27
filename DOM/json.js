a76aafb82647adca85bfeb1505ca4a81f1d46d53
Your Name <you@example.com>
2025-04-27 14:18:19 +0530

    learn Json

---
+++ DOM/json.js
@@ -0,0 +1,10 @@
+let myJson ={
+       "name":"harsh",
+       "class":"btech",
+       "rollno":30,
+       "isstudent":true,
+       "licence":null,
+       "bookName":["math","science","computerscience","computernetwork"] 
+}
+
+console.log(myJson.name);
